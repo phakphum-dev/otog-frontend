01d17739eb69e9a14c1d35274256d0fea6d1ddbb
Anon-136 <anonjin44@gmail.com>
2021-01-25 17:04:11 +0700

    :lipstick: add home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,32 @@
-import { Container } from '@chakra-ui/react'
+import {
+  Button,
+  Center,
+  Container,
+  Heading,
+  Img,
+  Stack,
+  Text,
+} from '@chakra-ui/react'
 
 export default function HomePage() {
-  return <Container>หน้าแรก</Container>
+  return (
+    <Container>
+      <Stack direction={{ base: 'column', md: 'row' }} spacing={16} mt={16}>
+        <Stack spacing={6} flex={1}>
+          <Heading as="h1" size="2xl">
+            Become a god of Competitive Programming
+          </Heading>
+          <Text color="gray.500" fontSize="md">
+            Code and Create algorithms efficiently.
+          </Text>
+          <Button width="150px">{'>_ Sign Up'}</Button>
+        </Stack>
+        <Center flex={1}>
+          <Img src="/computer.svg" width="100%" />
+        </Center>
+      </Stack>
+    </Container>
+  )
 }
 
 export { getServerSideProps } from '@src/theme/ColorMode'
