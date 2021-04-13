79fd1c6a04d19c75daa771883ba0ed0036cef984
Anon-136 <anonjin44@gmail.com>
2021-04-13 11:58:06 +0700

    :lock: move firebase secret to local environment

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,3 +2,15 @@ export const isDevelopment = process.env.NODE_ENV === 'development'
 export const isProduction = process.env.NODE_ENV === 'production'
 export const API_HOST = process.env.NEXT_PUBLIC_API_HOST
 export const CONTACT_LINK = process.env.NEXT_PUBLIC_CONTACT_LINK
+
+export const getFirebaseConfig = (base64: string | undefined): object => {
+  if (base64) {
+    const json = Buffer.from(base64, 'base64').toString('ascii')
+    return JSON.parse(json)
+  }
+  return {}
+}
+
+export const FIREBASE_CONFIG = getFirebaseConfig(
+  process.env.NEXT_PUBLIC_FIREBASE_CONFIG
+)
--- src/utils/firebase/index.ts
+++ src/utils/firebase/index.ts
@@ -1,18 +1,9 @@
 import firebase from 'firebase/app'
 import 'firebase/storage'
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyA595UmDskuDQUgzAiSKMizBCR1IyFf0Cc',
-  authDomain: 'otog-8240d.firebaseapp.com',
-  projectId: 'otog-8240d',
-  storageBucket: 'otog-8240d.appspot.com',
-  messagingSenderId: '133266075224',
-  appId: '1:133266075224:web:74301227cd432d8439e42d',
-  measurementId: 'G-SYNTM07HWX',
-}
+import { FIREBASE_CONFIG } from '../config'
 
 if (firebase.apps.length === 0) {
-  firebase.initializeApp(firebaseConfig)
+  firebase.initializeApp(FIREBASE_CONFIG)
 }
 
 const storage = firebase.storage()
