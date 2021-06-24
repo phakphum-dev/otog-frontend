79bff62649233aaad16908e2863860615fb0da3c
Anon-136 <anonjin44@gmail.com>
2021-06-24 17:45:04 +0700

    :sparkles: add submission page

--- src/pages/submission/all.tsx
+++ src/pages/submission/all.tsx
@@ -2,10 +2,13 @@ import { Button } from '@chakra-ui/button'
 import { PageContainer } from '@src/components/PageContainer'
 import { AllSubmissionTable } from '@src/components/SubmissionTable'
 import { Title, TitleLayout } from '@src/components/Title'
-import { useAuth } from '@src/api/AuthProvider'
+import { getUserData, useAuth } from '@src/api/AuthProvider'
 import Head from 'next/head'
 import NextLink from 'next/link'
 import { FaTasks } from 'react-icons/fa'
+import { parseCookies } from 'nookies'
+import { getServerSideCookies } from '@src/api'
+import { GetServerSideProps } from 'next'
 
 export default function SubmissionPage() {
   const { isAuthenticated } = useAuth()
@@ -16,16 +19,23 @@ export default function SubmissionPage() {
       </Head>
       <TitleLayout>
         <Title icon={FaTasks}>ผลตรวจรวม</Title>
-        {isAuthenticated && (
+        {/* {isAuthenticated && (
           <NextLink href="/submission" passHref>
             <Button as="a">ผลตรวจของคุณ</Button>
           </NextLink>
-        )}
+        )} */}
       </TitleLayout>
       <AllSubmissionTable />
     </PageContainer>
   )
 }
 
-export { getNotFound as getServerSideProps } from '@src/api'
 // export { getServerSideCookies as getServerSideProps } from '@src/api'
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { accessToken = null } = parseCookies(context)
+  const userData = getUserData(accessToken)
+  if (userData?.role === 'admin') {
+    return getServerSideCookies(context)
+  }
+  return { notFound: true }
+}
