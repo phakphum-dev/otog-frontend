48f335c7500831a4276601ad9c08359e5c5647ec
Anon-136 <anonjin44@gmail.com>
2023-05-11 17:22:30 +0700

    :lipstick: fix overflow table

--- src/components/layout/PageContainer.tsx
+++ src/components/layout/PageContainer.tsx
@@ -16,9 +16,9 @@ export const PageContainer = ({
     <div
       className={clsx(
         'mx-auto w-full flex-1 px-4',
-        maxSize === 'md' && 'sm:max-w-screen-sm md:max-w-screen-md',
+        maxSize === 'md' && 'max-w-screen-sm md:max-w-screen-md',
         maxSize === 'lg' &&
-          'sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg',
+          'max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg',
         className
       )}
     >
