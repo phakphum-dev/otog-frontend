6ab9cfca30dd11a3e7338b6b3300f0add4a5c124
ttamx <teetat.tata@gmail.com>
2023-05-25 13:43:30 +0700

    🔧 fix submissions bg color

--- src/theme/useStatusColor.ts
+++ src/theme/useStatusColor.ts
@@ -3,9 +3,18 @@ import { Submission } from '@src/submission/types'
 export function getBgColor(submission: Submission | undefined | null) {
   if (submission) {
     if (submission.status === 'accept') {
-      return 'bg-accept-50 dark:bg-accept-900 hover:bg-accept-100 active:bg-accept-200 dark:hover:bg-accept-800 active:dark:bg-accept-700'
+      return 'bg-accept-50 dark:bg-accept-900'
+    }
+  }
+  return ''
+}
+
+export function getBgHoveredColor(submission: Submission | undefined | null) {
+  if (submission) {
+    if (submission.status === 'accept') {
+      return 'cursor-pointer hover:bg-accept-100 active:bg-accept-200 dark:hover:bg-accept-800 active:dark:bg-accept-700'
     } else {
-      return 'hover:bg-black/4 active:bg-black/8 dark:hover:bg-white/4 active:dark:bg-white/8'
+      return 'cursor-pointer hover:bg-black/4 active:bg-black/8 dark:hover:bg-white/4 active:dark:bg-white/8'
     }
   }
   return ''
